4ecf4ac4a31c613c8d7b19bed2a544f3c70c02ac
adelansari <adelansari.a@gmail.com>
2024-10-22 15:23:13 +0300

    Remove unused CSS styles and update landing page heading color for light and dark mode

--- client/src/components/pages/LandingPage.tsx
+++ client/src/components/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage: React.FC = () => {
   return (
     <div className='relative w-full min-h-screen overflow-auto'>
       <div className='absolute inset-0 flex flex-col items-center justify-center z-10 '>
-        <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-white text-center p-3 rounded-lg dark:text-black leading-tight'>
+        <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-black text-center p-3 rounded-lg dark:text-white leading-tight'>
           Druid Final Project
         </h1>
       </div>
