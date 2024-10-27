9255ae64e8634014e4704c01f3c8a9f64d217e50
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-10-27 04:04:28 +0200

    Add other input fields to the generated fileds by form module

--- server/web/modules/contact_form/src/Form/ContactUsForm.php
+++ server/web/modules/contact_form/src/Form/ContactUsForm.php
@@ -24,17 +24,41 @@ final class ContactUsForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
 
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+      '#required' => TRUE,
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#required' => TRUE,
+    ];
+    $form['company'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Company'),
+      '#required' => TRUE,
+    ];
+    $form['phone'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Phone'),
+      '#required' => TRUE,
+    ];
     $form['message'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Message'),
       '#required' => TRUE,
     ];
-
+    $form['checkbox'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('I accept the use of my personal data in accordance with Druid’s privacy statement.'),
+      '#required' => TRUE,
+    ];
     $form['actions'] = [
       '#type' => 'actions',
       'submit' => [
         '#type' => 'submit',
-        '#value' => $this->t('Send'),
+        '#value' => $this->t('Send Message'),
       ],
     ];
 
