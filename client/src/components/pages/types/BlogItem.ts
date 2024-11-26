0f0830c31f98efb492e194bc4cd2a8e54ca2fed9
adelansari <adelansari.a@gmail.com>
2024-11-26 12:18:21 +0400

    Refactor component and add new styles
    Co-authored-by: Toni Andström <toni_andstrom@hotmail.com>
    Co-authored-by: Hetti <hetti.ronnemaa@gmail.com>
    Co-authored-by: Sulaimon Ekundayo <topdandydot@gmail.com>

--- client/src/components/pages/types/BlogItem.ts
+++
@@ -1,9 +0,0 @@
-export interface BlogItem {
-  id: string;
-  field_blog_taxonomy?: { name: string }[];
-  field_hero_image?: { uri: { url: string }; meta: { alt?: string } };
-  field_author?: { display_name: string };
-  field_date_of_post?: string;
-  field_add_title?: string;
-  field_short_description?: { value: string };
-}
---
+++ client/src/types/BlogItem.ts
@@ -0,0 +1,27 @@
+export interface BlogItem {
+  id: string;
+  field_blog_taxonomy?: { name: string }[];
+  field_hero_image?: {
+    uri: {
+      url: string;
+    };
+    meta?: {
+      alt?: string;
+    };
+  };
+  field_date_of_post?: string;
+  field_author?: {
+    display_name?: string;
+  };
+  field_add_title?: string;
+  field_short_description?: {
+    value: string;
+  };
+  field_add_title_text_content_ima?: Array<{
+    id: string;
+    type: string;
+    field_add_title?: Array<{ value: string }>;
+    field_add_textfield?: { value: string };
+    field_add_link?: { uri: string; title?: string };
+  }>;
+}
